bc0db44bf06575bdb288c6fad3d656a56063615d
Zach Baskin <zachbas@gmail.com>
2017-04-22 15:22:58 -0400

    Revert "Revert "Added the intro""
    This reverts commit 5663259da71c2a7a7a541121d2a30bb7ae38b388.

---
+++ Goblin Text.py
@@ -0,0 +1,8 @@
+
+answer1 = input("Hello!, This game is called The Adventure, it is about a man who wants to find treasure, your goal is to find it. It's a dangerous world out there, a single wrong choice could kill you. Do you want to play? Y/N")
+
+if answer1 == Y:
+    print("Ok, lets start! You start in an armoury, pick a weapon")
+    weapon = input(" ")
+else:
+    print("ok")
